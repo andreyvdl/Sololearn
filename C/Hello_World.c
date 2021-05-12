0442e86d486e1c2aea57c541fedf57b100e898a1
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2021-05-12 12:30:10 -0300

    Delete Hello_World.c

--- C/Hello_World.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> //It's the library that contains "printf()" function.
-
-int main() { //It's the entry of the program, the "{}" indicates where the block starts and end.
-	printf("Hello, World!\n"); //The "\n" indicates a break line.
-	return 0; //"return" ends the process of "main", it should return 0 if the program runs ok, and any other number if failed.
-}
