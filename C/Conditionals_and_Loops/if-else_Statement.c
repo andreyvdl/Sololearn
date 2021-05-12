a4caf7f3c9026e5f0e30b9ec294fc5875340563b
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-12 13:38:41 -0300

    DAY03

---
+++ C/Conditionals_and_Loops/if-else_Statement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int	main() {
+	int	score = 89;
+
+	if (score >= 90)
+		printf("Top 10%%.\n");
+	else //The else statement say that: "if the if statement is false, then execute this".
+		printf("Less than 90.\n");
+
+	return 0;
+}
