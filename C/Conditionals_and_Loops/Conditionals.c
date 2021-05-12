27fef9893972e8a0a0b46515bd9abc2f6e8e3d86
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-12 13:18:30 -0300

    DAY03

---
+++ C/Conditionals_and_Loops/Conditionals.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	main() {
+	int	score = 89;
+
+	if (score > 75) //The "if" will check if the expression is true, if it is then it will enter to execute the statements.
+		printf("You passed.\n");
+
+	return 0;
+}
