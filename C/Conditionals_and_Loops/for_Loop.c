17e1612880e0eb4d123158caf26fa2decc80e573
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-14 11:34:26 -0300

    DAY05

---
+++ C/Conditionals_and_Loops/for_Loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main() {
+	int i;
+	int max = 10;
+
+	for (i = 0; i < max; i++){
+		printf("%d\n", i);
+	}
+	return 0;
+}
+
+/*
+ * The "for" works like "while" it will create a loop, but you define the start value of the increment and the condition to continue the loop, on the same line.
+ */ 
