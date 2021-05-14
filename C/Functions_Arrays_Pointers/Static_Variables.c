bf9d0998fb88972e2e12c9080b7c1490b9f771bc
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-14 14:26:24 -0300

    DAY05

---
+++ C/Functions_Arrays_Pointers/Static_Variables.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void	say_hello();
+
+int	main() {
+	int	i;
+
+	for (i = 0; i < 5; i++) {
+		say_hello();
+	}
+	return 0;
+}
+
+void	say_hello() {
+	static int	num_calls = 1;
+
+	printf("Hello number %d\n", num_calls);
+	num_calls++;
+}
+
+/*
+ * The "static" prefix before the "int" says that: "this variable won't be reseted after the execution of this function is over".
+ */
