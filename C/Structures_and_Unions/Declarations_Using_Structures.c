b6d028d35c6d7061357bc45d5a7b0455b7290777
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-06-01 15:55:03 -0300

    structure variables

---
+++ C/Structures_and_Unions/Declarations_Using_Structures.c
@@ -0,0 +1,13 @@
+struct student {
+	int age;
+	int grade;
+	char name[40];
+};
+
+struct student s1;
+struct student s2;
+
+/*
+ * You can declare variables to the structure, you need to put the keyword "struct" and the struct tag before the variable name.
+ * In the e.g. above the structure "student" has now variable "s1" and "s2".
+*/
