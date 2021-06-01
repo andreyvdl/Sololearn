3352610b1851a65223432d3e137ee0af1d5ff47c
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-06-01 15:42:11 -0300

    structures

---
+++ C/Structures_and_Unions/Structures.c
@@ -0,0 +1,12 @@
+struct course {
+	int id;
+	char title[40];
+	float hours;
+};
+
+/*
+ * A structure is a data type created by the user.
+ * A structure declaration has, the keyword "struct", the "structure tag", the curly braces and a list of variables called "members".
+ * Don't forget the ";" at the end of the structure.
+ * The data types can be anything, ints, floats, chars, arrays, pointers, strings, other structures etc.
+*/
