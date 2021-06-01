ff095fe9b2445f32ab777b9ab133fe373558096e
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-06-01 16:03:20 -0300

    V2

--- C/Structures_and_Unions/Declarations_Using_Structures.c
+++ C/Structures_and_Unions/Declarations_Using_Structures.c
@@ -4,10 +4,16 @@ struct student {
 	char name[40];
 };
 
-struct student s1;
+struct student s1 = {19, 9, "John"};
 struct student s2;
+s2 = (struct student) {22, 10, "Batman"};
+struct student s3
+= { .grade = 9, .age = 19, .name = "Andrey"};
 
 /*
  * You can declare variables to the structure, you need to put the keyword "struct" and the struct tag before the variable name.
  * In the e.g. above the structure "student" has now variable "s1" and "s2".
+ *
+ * In the e.g. above you can also se the 3 different ways of declaring info of the variable.
+ * You can do it after the declaration on the same line, calling the variable and by giving details to the variables inside of the structure.
 */
