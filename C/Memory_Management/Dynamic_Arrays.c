6db690f2d35e9e5fdaac6a8969d18c02b2b5cd92
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-10-03 02:11:05 -0300

    fix?

--- C/Memory_Management/Dynamic_Arrays.c
+++ C/Memory_Management/Dynamic_Arrays.c
@@ -13,4 +13,22 @@ int main() {
 	arr.size = 0;
 	arr.elements = calloc(1, sizeof(*arr.elements));
 	arr.cap = 1;
+
+
+	arr.elements = realloc(arr.elements, (5 + arr.cap) *
+	sizeof(*arr.elements));
+
+	if (arr.elements != NULL)
+		arr.cap += 5;
+
+	if (arr.size < arr.cap) {
+		arr.elements[arr.size] = 50;
+		arr.size++;
+	}
+	else
+		printf("Need to expand the array.");
+
+	free(arr);
+
+	return 0;
 }
