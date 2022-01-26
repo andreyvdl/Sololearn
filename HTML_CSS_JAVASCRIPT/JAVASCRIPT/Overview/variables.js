5541b414f62af743bd99f00817473f0d164ea46a
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-01-26 00:32:45 -0300

    starting pain in the a

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Overview/variables.js
@@ -0,0 +1,18 @@
+/*
+variables holds data values, it can be changed throughout the program;
+under this comment we are saying that the variable "x" will hold a "y" value (10);
+don't forget the "var before";
+some important rules on naming a variable are:
+The first character has ti be a letter, underscore or a dollar sign;
+The first character can't be a number;
+Name can't contain a mathematical or logical operator;
+The name can't have spaces;
+You can't you special symbols like #, %, etc;
+*/
+var x = 10;
+
+var x = 100;
+document.write(x);
+
+x = 42;
+document.write(x);
