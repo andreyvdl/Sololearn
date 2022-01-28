e5b2e9a00cd9a3924a4f566b0f9e2632ff1fb3c4
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-01-27 23:21:35 -0300

    starting basics

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Basic_Concepts/Assignment_Operators.js
@@ -0,0 +1,14 @@
+/*
+Assignment operators are used to make the code shorter:
+"x += y" equal to x = x + y;
+"x -= y" equal to x = x - y;
+"x *= y" equal to x = x * y;
+"x /= y" equal to x = x / y;
+"x %= y" equal to x = x % y;
+*/
+
+var x = 5;
+var y = 7;
+x -= y += 9;
+
+console.log(x);
---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Basic_Concepts/Math_Operators.js
@@ -0,0 +1,22 @@
+/*
+Arithmetic Operators:
+"+" addition 5 + 5 = 10;
+"-" subtraction 5 - 5 = 0;
+"*" multiplication 5 * 5 = 25
+"/" division 5 / 5 = 1;
+"%" modulus 5 % 5 = 0;
+"++" increment a = 5; a++; a -> 6 | before the variable is pre increment, after is a post increment;
+"--" decrement a = 5; a--; a -> 4 | before the variable is pre decrement, after is a post decrement;
+*/
+var x = 10 + 5;
+console.log(x);
+
+var y = x + 5 + 22 + 45 + 6548;
+console.log(y);
+
+/*
+You can also use "eval()" to get a result from a string expression;
+*/
+console.log(eval("10 * 20 + 8"));
+
+console.log(38 % 5);
