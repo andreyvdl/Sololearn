0d1905bda9eb302d6b1c23bdea5e719674c2b1c1
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-01-30 23:30:59 -0300

    end of basic

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Basic_Concepts/String_Operators.js
@@ -0,0 +1,7 @@
+/*
+You can concatenate two string by using a + sing;
+*/
+
+var mystring1 = "This is the first string, ";
+var mystring2 = "now they are the same string.";
+console.log(mystring1 + mystring2);
