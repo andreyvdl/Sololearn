fe3e282d14c949d4ac201256c59fa67348cc2ce5
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-02-01 00:30:42 -0300

    if this if that

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Conditionals_Loops/else.js
@@ -0,0 +1,17 @@
+/*
+If a if condition is false, you can run a else statement;
+*/
+
+var colorMode = false;
+
+if (colorMode == true) {
+	console.log("Light mode XO");
+}
+else {
+	console.log("Dark mode :)");
+}
+
+/*
+You can also do this;
+*/
+colorMode === false ? console.log("pm") : console.log("am");
---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Conditionals_Loops/if.js
@@ -0,0 +1,13 @@
+/*
+if is a conditional statement, it defines if a condition happens, the statements inside of the code block are read, if not it will pass;
+*/
+
+var myNum1 = 7;
+var myNum2 = 10;
+if (myNum1 < myNum2) {
+	console.log("JavaScript... one of C's son.");
+}
+
+if (myNum1 === myNum2) {
+	console.log("Not gonna work... unless..........");
+}
