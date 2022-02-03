081add7ed3bce3d7e2b07b028a37860319b1c78c
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-02-03 01:03:49 -0300

    loop loop loop loop loop loop loop loop loop

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Conditionals_Loops/for.js
@@ -0,0 +1,12 @@
+/*
+for loops are defined by 3 statements;
+the first will happen before starting the loop;
+the second is the condition to continue running the loop;
+the third will happen after ending the loop;
+*/
+
+var number;
+
+for (number = 3; number > 0; number--) {
+	console.log(number);
+}
---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Conditionals_Loops/while.js
@@ -0,0 +1,10 @@
+/*
+while loop is just a simple for loop, they just require a condition to continue the loop;
+*/
+
+var weekDays = 1;
+
+while (weekDays < 8) {
+	console.log(weekDays);
+	weekDays++;
+}
