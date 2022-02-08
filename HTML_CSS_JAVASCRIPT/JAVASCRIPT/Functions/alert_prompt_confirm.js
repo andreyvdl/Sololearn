dac040ff0380959539f4d5ab6df177dd53023905
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-02-07 23:52:28 -0300

    YOUR SOUL IS MINE

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Functions/alert_prompt_confirm.js
@@ -0,0 +1,18 @@
+/*
+alert will activate an alert on the webpage;
+prompt will activate an alert prompt on the webpage;
+*/
+
+alert("Warning!\nYou don't have choice... ONLY OK :)");
+var name = prompt("What's your name Mr/s?");
+alert("Ok so your name is " + name);
+var response = confirm("Do you want to cancel the contract?");
+if (response != true)
+{
+	alert("https://www.youtube.com/watch?v=UYGrQaReIL8");
+}
+else
+{
+	alert("https://www.youtube.com/watch?v=QUAItQmq-LU");
+}
+alert("goodbye :)");
