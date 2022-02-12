5a790efff98a4d801c805d404115aea60c0ecba2
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-02-11 23:54:26 -0300

    IDK

---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Core_Objects/arrays_and_methods.js
@@ -0,0 +1,12 @@
+/*
+arrays have some built-in properties, length is one of them;
+concat will concatenate 2 arrays;
+*/
+
+var c1 = ["C"];
+var c2 = ["C++", "C#"];
+
+console.log(c1.length);
+c1 = c1.concat(c2);
+console.log(c1.length);
+console.log(c1);
---
+++ HTML_CSS_JAVASCRIPT/JAVASCRIPT/Core_Objects/associative_arrays.js
@@ -0,0 +1,8 @@
+/*
+you can transform an array in object;
+*/
+
+var person = [];
+person.name = "João";
+person.age = 46;
+console.log(person.name);
