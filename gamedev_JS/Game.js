715c8f06a23ded847eaeb3371b6707545882c3fa
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-02-10 10:43:13 -0300

    seria mais seguro usar TS

--- gamedev_JS/Game.js
+++ gamedev_JS/Game.js
@@ -1,8 +1,16 @@
 window.onload = function() {
 	let btn = document.getElementById("jump");
 	let count = 0;
+	let	canvas = document.getElementById("canvas");
+	let	context = canvas.getContext("2d");
+	let	x = 300;
+	let	y = 350;
 
 	btn.onclick = function() {
 		count += 1;
 	}
+
+	context.arc(x, y, 50, 0, 2 * Math.PI);
+	context.fillStyle = "red";
+	context.fill();
 }
