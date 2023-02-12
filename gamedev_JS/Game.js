75f5acef0cb1d6b5622a3576c3618df84565a837
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-02-11 21:16:40 -0300

    setInterval(draw, 500) === 5 seconds

--- gamedev_JS/Game.js
+++ gamedev_JS/Game.js
@@ -8,6 +8,15 @@ window.onload = function() {
 
 	btn.onclick = function() {
 		count += 1;
+		y -= 25;
+		context.clearRect(0, 0, 600, 400);
+		context.beginPath();
+		context.art(x, y, 50, 0, 2 * Math.PI);
+		context.fillStyle = "red";
+		context.fill();
+		context.font = '25px Arial';
+		context.fillStyle = 'white';
+		context.fillText("Count: " + count, 10, 30);
 	}
 
 	context.arc(x, y, 50, 0, 2 * Math.PI);
